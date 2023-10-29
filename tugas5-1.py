de67a1095a18806663e4b7efd06a02276151226f
Salamahmutmainnah <146900571+Salamahmutmainnah@users.noreply.github.com>
2023-10-29 14:34:02 +0700

    Update tugas5-1.py

--- tugas5-1.py
+++ tugas5-1.py
@@ -1,7 +1,7 @@
 kendaraan1 = ["Avanza," "Alphard", "Mobil", "Toyota", 1500]
 print(kendaraan1)
 
-kendaraan1 = kendaraan1 + ["hitam", 4, 450_000_000]
+kendaraan1 = kendaraan1 + ["hitam", 4, 785_000_000]
 print(kendaraan1)
 
 import sys
